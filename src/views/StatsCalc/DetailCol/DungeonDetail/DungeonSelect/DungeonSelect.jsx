754540b1327cc00d5c53d5d9691a544e60b85757
junlico <sean.junlico@gmail.com>
2019-04-21 16:01:19 -0700

    high dungeon info in calc mode

--- src/views/StatsCalc/DetailCol/DungeonDetail/DungeonSelect/DungeonSelect.jsx
+++ src/views/StatsCalc/DetailCol/DungeonDetail/DungeonSelect/DungeonSelect.jsx
@@ -5,26 +5,37 @@ import { dungeonInfo } from 'data';
 import { Select } from 'components';
 
 class DungeonSelect extends React.PureComponent {
+  state = {
+    dungeonOptions: Object.keys(dungeonInfo),
+  };
+
   render() {
     const { dungeon, onChange } = this.props;
-    const dungeonOptions = Object.keys(dungeonInfo);
     // TODO hide dungeon info in calc mode, show it in simc
     // const info = dungeonInfo[dungeon];
+    // <div className="flex gutter-top">
+    //   <InputNumber
+    //     label="STR"
+    //     value={info.STR}
+    //     disabled={true}
+    //     onChange={onChange}
+    //   />
+
+    //   <InputNumber
+    //     label="multi"
+    //     value={info.multi}
+    //     disabled={true}
+    //     onChange={onChange}
+    //   />
+    // </div>;
+
     return (
-      <>
-        <Select
-          label="dungeon"
-          value={dungeon}
-          options={dungeonOptions}
-          onChange={onChange}
-        />
-
-        {/* <div className="flex gutter-top">
-          <InputNumber label="STR" value={info.STR} disabled={true} onChange={onChange} />
-
-          <InputNumber label="multi" value={info.multi} disabled={true} onChange={onChange} />
-        </div> */}
-      </>
+      <Select
+        label="dungeon"
+        value={dungeon}
+        options={this.state.dungeonOptions}
+        onChange={onChange}
+      />
     );
   }
 }
