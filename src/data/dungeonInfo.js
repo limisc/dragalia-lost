c965c03485c285fcdee7bfe30eb28c1ad9701814
junlico <sean.junlico@gmail.com>
2019-04-21 15:54:35 -0700

    locales

--- src/data/dungeonInfo.js
+++ src/data/dungeonInfo.js
@@ -1,21 +1,21 @@
 const dungeonInfo = {
   hms: {
     STR: 7230,
-    multi: 3.6,
+    mult: 3.6,
     element: 'Wind',
     adventage: 'Water',
     disadvantage: 'Flame',
   },
   hbh: {
     STR: 7230,
-    multi: 4.8,
+    mult: 4.8,
     element: 'Flame',
     adventage: 'Wind',
     disadvantage: 'Water',
   },
   hmc: {
     STR: 7230,
-    multi: 2.75,
+    mult: 2.75,
     element: 'Water',
     adventage: 'Flame',
     disadvantage: 'Wind',
