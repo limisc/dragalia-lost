5cf454432052be3a899bef07300154e8c00adf17
junlico <sean.junlico@gmail.com>
2019-04-21 11:39:09 -0700

    Settings - SetLang

---
+++ src/views/StatsCalc/DetailCol/Settings/Settings.jsx
@@ -0,0 +1,14 @@
+//@flow
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import SetLang from './SetLang';
+
+const Settings = () => {
+  return (
+    <div className="flex">
+      <SetLang />
+    </div>
+  );
+};
+
+export default Settings;
