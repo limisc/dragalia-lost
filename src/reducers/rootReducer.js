78490b9951c8e761a8047fae2ecbbb025528b022
junlico <sean.junlico@gmail.com>
2019-04-25 13:50:07 -0700

    focusReducer & statsReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,9 +1,23 @@
+import { actionTypes, getField } from '../actions';
 import filterReducer from './filterReducer';
+import statsReducer from './statsReducer';
 
-const rootReducer = ({ field, filters, stats }, action) => {
+const focusReducer = (focusKey, action) => {
+  if (action.type === actionTypes.SELECT_FOCUS) {
+    return action.statsKey;
+  }
+
+  return focusKey;
+};
+
+const rootReducer = ({ focusKey, filters, stats }, action) => {
+  const newFocus = focusReducer(focusKey, action);
+  const newStats = statsReducer(stats, action);
   return {
-    field,
-    filters: filterReducer(filters, action, stats),
+    focusKey: newFocus,
+    stats: newStats,
+    field: getField(newFocus),
+    filters: filterReducer(filters, action, newStats),
   };
 };
 
