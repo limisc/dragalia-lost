6b3a50a33383773f1b6cde63649c5dd6803e8de2
junlico <sean.junlico@gmail.com>
2019-04-25 14:04:44 -0700

    add StatsCol

--- src/views/StatsCalc/StatsCalc.js
+++ src/views/StatsCalc/StatsCalc.js
@@ -1,9 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React, { Fragment } from 'react';
+import SetCol from './SetCol';
+import StatsCol from './StatsCol';
+
 
 const StatsCalc = () => {
   return (
     <Fragment>
+      <div className="column" />
+      <StatsCol />
+      <SetCol />
     </Fragment>
   );
 };
