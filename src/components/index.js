1e62fd43d29fef33faf8dd2872870a3b25b7ef2f
junlico <sean.junlico@gmail.com>
2019-04-25 04:09:17 -0700

    Image

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ import Context from './Context';
 import ThemeProvider from './ThemeProvider';
 import withTheme from './withTheme';
 import Select from './Select';
-export { Context, ThemeProvider, withTheme, Select };
+import Image from './Image';
+export { Context, ThemeProvider, withTheme, Select, Image };
