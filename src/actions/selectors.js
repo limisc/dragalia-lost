236ffeb40343b341ec3d3642e7bbda725e7e9018
junlico <sean.junlico@gmail.com>
2019-04-25 14:05:28 -0700

    update actions

--- src/actions/selectors.js
+++ src/actions/selectors.js
@@ -1,4 +1,5 @@
 import locales from 'locales/default';
+import { dataDict, limit } from 'data';
 
 export const translate = (content, lang = 'en') => {
   if (locales[content]) {
@@ -6,4 +7,39 @@ export const translate = (content, lang = 'en') => {
   }
 
   return content;
+};
+
+export const getField = key => {
+  if (key === 'wyrmprint1' || key === 'wyrmprint2') {
+    return 'wyrmprint';
+  }
+
+  return key;
+};
+
+export const getItem = (statsKey, id) => {
+  const field = getField(statsKey);
+  if (dataDict[field]) {
+    return dataDict[field][id];
+  }
+
+  return null;
+};
+
+export const getLimit = (key, rarity, unbind = 4) => {
+  switch (key) {
+    case 'slime':
+      return limit[key];
+    case 'adventurer':
+    case 'mana':
+      return limit[key][rarity] || 0;
+    case 'weapon':
+    case 'wyrmprint1':
+    case 'wyrmprint2':
+    case 'dragon':
+      const field = getField(key);
+      return limit[field][rarity] ? limit[field][rarity][unbind] : 0;
+    default:
+      return 30;
+  }
 };
