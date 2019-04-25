4542378ba3b6e96eca92612d75105d5b5d94ce92
junlico <sean.junlico@gmail.com>
2019-04-25 02:54:48 -0700

    index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Redirect, Router, Route, Switch } from 'react-router-dom';
+import { history, store } from 'store';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const DragaliaLost = () => (
+  <Provider store={store}>
+    <Router history={history}>
+      <Switch>
+        <Route path="/:page/:lang(en|ja|zh)?" component={App} />
+        <Redirect to="/stats" />
+      </Switch>
+    </Router>
+  </Provider>
+);
+
+ReactDOM.render(<DragaliaLost />, document.getElementById('root'));
