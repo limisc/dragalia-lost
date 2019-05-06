0b995effcec711df773ede9f74258732b05803db
junlico <sean.junlico@gmail.com>
2019-05-06 16:29:16 -0700

    CustomRoute

--- src/index.js
+++ src/index.js
@@ -2,15 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Redirect, Router, Route, Switch } from 'react-router-dom';
-import { history, store } from 'store';
-import App from './App';
+import { history, store } from './store';
+import { ThemeProvider } from './components';
+import { Header } from './views';
+import routers from './routers';
 import './index.css';
+
+const CustomRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props => (
+      <ThemeProvider>
+        <Header />
+        <main>
+          <Component {...props} />
+        </main>
+      </ThemeProvider>
+    )}
+  />
+);
 
 const DragaliaLost = () => (
   <Provider store={store}>
     <Router history={history}>
       <Switch>
-        <Route path="/:page/:lang(en|ja|zh)?" component={App} />
+        {routers.map(r => (
+          <CustomRoute key={r.id} path={r.path} component={r.component} />
+        ))}
         <Redirect to="/stats" />
       </Switch>
     </Router>
