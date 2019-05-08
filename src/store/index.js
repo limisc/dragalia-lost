0f7be6deccf7163a49dfa2d2fedbc25733b291f7
junlico <sean.junlico@gmail.com>
2019-05-07 21:58:02 -0700

    UPDATE store

--- src/store/index.js
+++ src/store/index.js
@@ -17,18 +17,4 @@ const refs = {
   searchBar: createRef(),
 };
 
-const selectOptions = {
-  mana: {
-    '3': ['30', '20', '10', '0'],
-    '4': ['40', '30', '20', '10', '0'],
-    '5': ['50', '45', '40', '30', '20', '10', '0'],
-  },
-  curRarity: {
-    '3': ['5', '4', '3'],
-    '4': ['5', '4'],
-    '5': ['5'],
-  },
-  unbind: ['4', '3', '2', '1', '0'],
-};
-
-export { history, statsKeys, refs, state, store, selectOptions };
+export { history, statsKeys, refs, state, store };
