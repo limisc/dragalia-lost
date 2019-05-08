6ef82b6fe41ea8afbb39e84dde63d50079984d1a
junlico <sean.junlico@gmail.com>
2019-05-07 21:58:58 -0700

    SetBtns toggle stats/dungeon

--- src/views/StatsCalc/DetailCol/SetBtns.js
+++ src/views/StatsCalc/DetailCol/SetBtns.js
@@ -2,27 +2,33 @@
 import React, { useContext } from 'react';
 import { connect } from 'react-redux';
 import { Button } from '@material-ui/core';
+import { ExpandLess, ExpandMore } from '@material-ui/icons';
+import classNames from 'classnames';
 import { Context } from 'components';
 import { translate, resetAll } from 'actions';
-import { ExpandLess, ExpandMore } from '@material-ui/icons';
 
-const SetBtns = props => {
-  const { expand, expandDisabled, toggleExpand, resetAll } = props;
+const SetBtns = ({ expand, expandDisabled, setExpand, resetAll }) => {
   const { lang } = useContext(Context);
+  const switchBtn = classNames({ 'toggle-switch': !expandDisabled });
 
   return (
     <div className="set-btns flex">
-      <Button className="col-3" variant="contained" disabled>
-        mode
-      </Button>
-      <Button
-        className="col-3"
-        variant="contained"
-        disabled={expandDisabled}
-        onClick={toggleExpand}
-      >
-        {expand ? <ExpandLess /> : <ExpandMore />}
-      </Button>
+      <span className="col-3" />
+
+      <div className="toggle col-3">
+        <input
+          type="checkbox"
+          id="simc"
+          className="toggle-checkbox"
+          disabled={expandDisabled}
+          checked={expand}
+          onChange={e => setExpand(e.target.checked)}
+        />
+        <label className="toggle-label" htmlFor="simc">
+          <span className="toggle-inner" />
+          <span className={switchBtn} />
+        </label>
+      </div>
 
       <Button className="col-3" variant="contained" onClick={resetAll}>
         {translate('reset', lang)}
