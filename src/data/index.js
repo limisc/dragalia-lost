06acfd590c60ec5f90601fd726300d729ac4aa39
junlico <sean.junlico@gmail.com>
2019-06-04 17:12:06 -0700

    facilityMaterial.js

--- src/data/index.js
+++ src/data/index.js
@@ -2,6 +2,7 @@ import dataList from './dataList';
 import dataDict from './dataDict';
 import limit, { values } from './limit';
 import facilities from './facilities';
+import { facilityMaterial, imageMap } from './facilityMaterial';
 import dungeonInfo from './dungeonInfo';
 import elements from './elements';
 import equipments from './equipments';
@@ -13,10 +14,12 @@ export {
   dataList,
   dataDict,
   dungeonInfo,
-  facilities,
   elements,
   equipments,
   exValues,
+  facilities,
+  facilityMaterial,
+  imageMap,
   limit,
   mightDict,
   values,
