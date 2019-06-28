c250570a73d2e33abbbaa28612c3f1d9c8229a66
junlico <sean.junlico@gmail.com>
2019-06-27 22:58:35 -0700

    change to rsc

--- src/views/StatsCalc/StatsCol/StatsCol.js
+++ src/views/StatsCalc/StatsCol/StatsCol.js
@@ -4,32 +4,20 @@ import { connect } from 'react-redux';
 import { refs, statsKeys } from 'store';
 import StatsField from './StatsField';
 
-class SetStats extends React.Component {
-  render() {
-    const { stats } = this.props;
-    return (
-      <div className="column" ref={refs.statsField}>
-        {statsKeys.map(statsKey => {
-          const id = stats[statsKey] ? stats[statsKey].id : statsKey;
-          return <StatsField key={id} statsKey={statsKey} />;
-        })}
-      </div>
-    );
-  }
-}
+const SetStats = ({ stats }) => {
+  return (
+    <div className="column" ref={refs.statsField}>
+      {statsKeys.map(statsKey => {
+        const item = stats[statsKey];
+        const { id = statsKey } = item || {};
+        return <StatsField key={id} statsKey={statsKey} item={item} />;
+      })}
+    </div>
+  );
+};
 
 const mapStateToProps = ({ stats }) => {
   return { stats };
 };
 
-const mapDispatchToProps = dispatch => {
-  return {
-    // TODO clear
-    //: () => dispatch(),
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(SetStats);
+export default connect(mapStateToProps)(SetStats);
