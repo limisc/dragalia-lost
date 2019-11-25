fca889ede807ad7f41a1aa3ee736101774223824
junlico <sean.junlico@gmail.com>
2019-11-25 14:00:20 -0800

    Random select adventurer at mounted

--- src/pages/Stats/Stats.jsx
+++ src/pages/Stats/Stats.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { selectItem } from 'actions';
+import content from 'data';
 import Col1 from './Col1';
 import Col2 from './Col2';
 import Col3 from './Col3';
 
-function Stats() {
+function Stats({ selectItem }) {
   const { lang = 'en' } = useParams();
+
+  useEffect(() => {
+    const keys = Object.keys(content.adventurer);
+    const key = Math.floor(keys.length * Math.random());
+    const item = content.adventurer[keys[key]];
+    selectItem('adventurer', item);
+  }, [selectItem]);
+
   return (
     <main id="stats">
       <Col1 lang={lang} />
@@ -15,4 +26,6 @@ function Stats() {
   );
 }
 
-export default Stats;
+const actionCreators = { selectItem };
+
+export default connect(null, actionCreators)(Stats);
