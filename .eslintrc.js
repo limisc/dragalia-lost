320f88eaf628ed3400c5dbb43f796442cc7a95f6
junlico <sean.junlico@gmail.com>
2019-12-19 15:24:36 -0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,14 +10,16 @@ module.exports = {
   parser: 'babel-eslint',
   rules: {
     'max-len': [1, { code: 80, tabWidth: 2, ignoreComments: true }],
-    "jsx-a11y/label-has-associated-control": [ 2, {
-      "labelAttributes": ["label"],
-      "depth": 3,
-    }],
+    'jsx-a11y/label-has-associated-control': [
+      2,
+      {
+        labelAttributes: ['label'],
+        depth: 3,
+      },
+    ],
     'react/prop-types': 0,
     'no-console': 0,
     'no-shadow': 0,
-
   },
   settings: {
     'import/resolver': {
