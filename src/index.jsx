7f94c9a2a255d6240f1eae98373812fcb4b88b4f
junlico <sean.junlico@gmail.com>
2020-01-03 08:52:33 -0800

    remove serviceWorker

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,6 @@ import smoothscroll from 'smoothscroll-polyfill';
 import store from './store';
 import Routes from './Routes';
 import './styles/styles.scss';
-import * as serviceWorker from './serviceWorker';
 
 smoothscroll.polyfill();
 
@@ -21,8 +20,3 @@ function App() {
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
